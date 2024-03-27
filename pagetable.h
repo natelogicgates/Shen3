6a9ed4fe75d7eb46d2439bec7fd62bbf30702d38
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2024-03-27 13:30:26 -0700

    Update pagetable.h

--- pagetable.h
+++ pagetable.h
@@ -1,1 +1,32 @@
+#ifndef PAGETABLE_H
+#define PAGETABLE_H
+
+#include <vector>
+#include <optional>
+
+class PageTableEntry {
+public:
+    bool valid = false;
+    unsigned int frameNumber = 0;
+};
+
+class Level {
+public:
+    std::vector<PageTableEntry> entries;
+    Level(int size);
+};
+
+class PageTable {
+public:
+    PageTable(int levels, int entriesPerLevel);
+    bool insert(unsigned int virtualAddress, unsigned int frameNumber);
+    std::optional<unsigned int> search(unsigned int virtualAddress);
+    bool remove(unsigned int virtualAddress);
+private:
+    std::vector<Level> tableLevels;
+    int entriesPerLevel;
+    unsigned int extractBits(unsigned int value, int startBit, int numBits);
+};
+
+#endif
 
