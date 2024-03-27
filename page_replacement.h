3bdff264ae3e14f53030168e8ccee3324f92afd5
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2024-03-27 13:35:38 -0700

    Update page_replacement.h

--- page_replacement.h
+++ page_replacement.h
@@ -1,1 +1,26 @@
+#ifndef PAGE_REPLACEMENT_H
+#define PAGE_REPLACEMENT_H
+
+#include <vector>
+#include <optional>
+
+struct Page {
+    unsigned int pageNumber;
+    unsigned int accessFrequency;
+    unsigned long lastAccessTime;
+    Page(unsigned int pageNum) : pageNumber(pageNum), accessFrequency(0), lastAccessTime(0) {}
+};
+
+class PageReplacement {
+public:
+    PageReplacement();
+    void accessPage(unsigned int pageNumber);
+    std::optional<unsigned int> replacePage(); // Returns the page number of the replaced page, if any
+    void agePages();
+private:
+    std::vector<Page> pages;
+    unsigned long currentTime;
+};
+
+#endif
 
