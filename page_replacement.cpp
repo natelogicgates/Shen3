cfb7048754986904df1bb150b4d64557fc1a209f
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2024-03-28 16:23:55 -0700

    Update page_replacement.cpp

--- page_replacement.cpp
+++ page_replacement.cpp
@@ -2,23 +2,27 @@
 #include <algorithm> // For std::min_element
 #include <iostream>
 
-PageReplacement::PageReplacement(unsigned int bitstringUpdateInterval, unsigned int numFrames) 
-    : currentTime(0), updateInterval(bitstringUpdateInterval), frameCount(numFrames), allocatedFrames(0) {}
-
+PageReplacement::PageReplacement(unsigned int maxFrames, unsigned int bitstringUpdateInterval)
+    : currentTime(0), updateInterval(bitstringUpdateInterval), maxFrames(maxFrames), allocatedFrames(0) {}
 
 void PageReplacement::accessPage(unsigned int pageNumber) {
-    // Increment access frequency or add new page if it does not exist
     bool pageFound = false;
-    for(auto &page : pages) {
-        if(page.pageNumber == pageNumber) {
+    for (auto &page : pages) {
+        if (page.pageNumber == pageNumber) {
             page.accessFrequency = std::min(page.accessFrequency + 1, (1U << 16) - 1); // Prevent overflow
             page.lastAccessTime = currentTime;
             pageFound = true;
             break;
         }
     }
-    if(!pageFound) {
-        pages.push_back(Page(pageNumber));
+    if (!pageFound) {
+        if (isFull()) {
+            auto frameNumber = replacePage().value(); // Get the frame number of the page to be replaced
+            pages.push_back(Page(pageNumber, frameNumber)); // Use the same frame for the new page
+        } else {
+            pages.push_back(Page(pageNumber, allocatedFrames)); // Allocate a new frame
+            allocatedFrames++;
+        }
     }
     currentTime++;
     if (currentTime % updateInterval == 0) {
@@ -27,52 +31,34 @@ void PageReplacement::accessPage(unsigned int pageNumber) {
 }
 
 std::optional<unsigned int> PageReplacement::replacePage() {
-    if(pages.empty()) {
+    if (pages.empty()) {
         return std::nullopt;
     }
-    // Find the page with the lowest frequency for replacement
     auto victim = std::min_element(pages.begin(), pages.end(), [](const Page &a, const Page &b) {
         return a.accessFrequency < b.accessFrequency || (a.accessFrequency == b.accessFrequency && a.lastAccessTime < b.lastAccessTime);
     });
-    unsigned int victimPageNumber = victim->pageNumber;
+    unsigned int frameNumber = victim->frameNumber;
     pages.erase(victim); // Remove the victim page from the list
-    return victimPageNumber; // Return the page number of the replaced (victim) page
+    return frameNumber; // Return the frame number of the replaced (victim) page
 }
 
 void PageReplacement::agePages() {
-    // Periodically called to age the pages
-    for(auto &page : pages) {
+    for (auto &page : pages) {
         page.accessFrequency >>= 1; // Right shift to simulate aging
     }
+}
+
 bool PageReplacement::isFull() const {
     return allocatedFrames >= maxFrames;
 }
 
 unsigned int PageReplacement::allocateFrame(unsigned int vpn) {
     if (isFull()) {
-        throw std::runtime_error("No free frames available");
+        auto frameNumber = replacePage().value(); // Get the frame number of the page to be replaced
+        pages.push_back(Page(vpn, frameNumber)); // Use the same frame for the new page
+        return frameNumber;
     } else {
-        allocatedFrames++;
-        auto evictedVpn = replacePage();
-        // Find and update the evicted page with the new VPN
-        for (auto &page : pages) {
-            if (page.pageNumber == evictedVpn.value()) {
-                page.pageNumber = vpn;
-                return page.frameNumber; // Reuse the frame of the evicted page
-            }
-        }
-      return allocatedFrames - 1;  
+        pages.push_back(Page(vpn, allocatedFrames)); // Allocate a new frame
+        return allocatedFrames++;
     }
-
-unsigned int PageReplacement::evictPage() {
-    if (pages.empty()) {
-        throw std::runtime_error("No pages to evict");
-    }
-    auto victim = std::min_element(pages.begin(), pages.end(), [](const Page &a, const Page &b) {
-        return a.accessFrequency < b.accessFrequency || (a.accessFrequency == b.accessFrequency && a.lastAccessTime < b.lastAccessTime);
-    });
-    unsigned int victimPageNumber = victim->pageNumber;
-    pages.erase(victim);
-    allocatedFrames--; // Assume frame is now free
-    return victimPageNumber;
 }
